16aa59ee208a9ddce9fc7b854d30aaa846034cd1
Irim212 <piwo855@gmail.com>
2018-05-30 14:42:29 +0200

    Enemy Start

--- Dragon Ball/Assets/EnemyThrash.cs
+++ Dragon Ball/Assets/EnemyThrash.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class EnemyThrash : MonoBehaviour {
 
+    [System.Serializable]
+    public class EnemyStat
+    {
+        public static int level = 1;
+        public static int maxHp = 100 * level;
+        public static int currentHp;
+        public static int damage;
+
+        private Animator anim;
+        private Rigidbody2D rb;
+    }
+
 	// Use this for initialization
 	void Start () {
 		
